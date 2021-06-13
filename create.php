cacaab9b19114c37bc9d2354634951affb074f84
Sahrul-ux <sahrulrajab11@gmail.com>
2021-06-13 11:38:23 +0700

    membuat fungsi submit pada file create.php

--- create.php
+++ create.php
@@ -20,61 +20,82 @@
 </head>
 <h3>CRUD Sederhana</h3>
 
-<form method="post" enctype="multipart/form-data">
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td><input type="text" name="nama" required></td>
-        </tr>
-        <tr>
-            <td>Alamat</td>
-            <td><input type="text" name="alamat" required></td>
-        </tr>
-        <tr>
-            <td>Tempat</td>
-            <td><input type="text" name="tempat" required></td>
-        </tr>
-        <tr>
-            <td>Tanggal Lahir</td>
-            <td><input type="text" name="ttl" id="datepicker" required></td>
-        </tr>
-        <tr>
-            <td>Agama</td>
-            <td>
-                <select name="agama" required>
-                    <option>-- Pilih Agama --</option>
-                    <option value="islam">islam</option>
-                    <option value="kristen">kristen</option>
-                    <option value="katolik">katolik</option>
-                    <option value="budha">budha</option>
-                    <option value="hindu">hindu</option>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>No. Handphone</td>
-            <td><input type="number" name="no_hp" required></td>
-        </tr>
-        <tr>
-            <td>Email</td>
-            <td><input type="text" name="email" required></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-            <td><input type="password" name="password" required></td>
-        </tr>
-        <tr>
-            <td>Foto</td>
-            <td><input type="file" name="foto" required></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Simpan" name="submit"></td>
-            <td><input type="reset" value="Reset"></td>
-        </tr>
-    </table>
-</form>
-
 <body>
+    <form method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama" required></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td><input type="text" name="alamat" required></td>
+            </tr>
+            <tr>
+                <td>Tempat</td>
+                <td><input type="text" name="tempat" required></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td><input type="text" name="ttl" id="datepicker" required></td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>
+                    <select name="agama" required>
+                        <option>-- Pilih Agama --</option>
+                        <option value="islam">islam</option>
+                        <option value="kristen">kristen</option>
+                        <option value="katolik">katolik</option>
+                        <option value="budha">budha</option>
+                        <option value="hindu">hindu</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>No. Handphone</td>
+                <td><input type="number" name="no_hp" required></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" name="email" required></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password" required></td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td><input type="file" name="foto" required></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Simpan" name="submit"></td>
+                <td><input type="reset" value="Reset"></td>
+            </tr>
+        </table>
+    </form>
+
+    <?php
+    include 'koneksi.php';
+    if (isset($_POST['submit'])) {
+        $nama = $_POST['nama'];
+        $alamat = $_POST['alamat'];
+        $tempat = $_POST['tempat'];
+        $ttl = $_POST['ttl'];
+        $agama = $_POST['agama'];
+        $no_hp = $_POST['no_hp'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $foto = $_FILES['foto']['name'];
+
+        $tmpFoto = $_FILES['foto']['tmp_name'];
+
+        $namaFoto = $nama . '-' . $foto;
+    }
+    ?>
+
+
 
 </body>
 
